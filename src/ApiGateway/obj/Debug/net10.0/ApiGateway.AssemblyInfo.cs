f20754d489f79d61eb712c38438d31a0cb761e2f
45000794Ndlakuse <ndlakusethamsanqa@gmail.com>
2026-09-03 17:37:41 +0200

    Logs

--- src/ApiGateway/obj/Debug/net10.0/ApiGateway.AssemblyInfo.cs
+++ src/ApiGateway/obj/Debug/net10.0/ApiGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+550871af6442d8b4e83de01caea2f161e6389552")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b21aaca789832bc4d9740c6cec82026b9e3a3ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
